99e6e6c6f9e01d4f02178eec460fb7c0268b7ba5
Anthony Berg <smyalygames@gmail.com>
2025-10-01 16:16:58 +0200

    fix(ensemble): add annotation import for type hinting

--- src/gpuocean/ensembles/OceanModelEnsemble.py
+++ src/gpuocean/ensembles/OceanModelEnsemble.py
@@ -22,6 +22,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+from __future__ import annotations
 from typing import TYPE_CHECKING
 import logging
 import gc
